815e1740e58520af0bfa746906d5bca274d42212
victor braz <victor.90braz@gmail.com>
2024-01-25 09:20:26 +0100

    create class

---
+++ FirstThousandPoints.php
@@ -0,0 +1,37 @@
+<?php
+
+class FirstThousandPoints
+{
+    public function name()
+    {
+        return 'First Thousand Points';
+    }
+
+    public function icon()
+    {
+        return 'first_thousand_points.png';
+    }
+
+    public function qualifier($user)
+    {
+
+    }
+}
+
+class FirstBestAnswer
+{
+    public function name()
+    {
+        return 'First Best Answer';
+    }
+
+    public function icon()
+    {
+        return 'first_best_answer.png';
+    }
+
+    public function qualifier($user)
+    {
+
+    }
+}
